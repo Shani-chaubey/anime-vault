2a80c4a3931c8232fe2e23e5bd72143211e2910c
Himanshu Chaturvedi <121288199+Shani-chaubey@users.noreply.github.com>
2024-09-25 12:59:38 +0530

    A Anime Vault with infinite scroll

---
+++ app/action.js
@@ -0,0 +1,11 @@
+import AnimeCard from "@/components/AnimeCard"
+
+export const fetchAnime = async(page=1) => {
+
+    const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=4&oder=popularity`)
+    const data = await response.json()
+    
+  return data.map((item, index) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ))
+}
---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import { DM_Sans } from "next/font/google";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+import "./globals.css";
+import { Metadata } from 'next';
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const Metadataetadata = {
+  title: "Anime Vault",
+  description: "Your favorite anime, all in one place.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
